0a18f77d533d0fbb66503e9f680e87bb549a7c37
Scott Campit <scampit@umich.edu>
2020-02-04 12:54:40 -0500

    Enabled basic query searching. Now need to figure out more sophisticated ways for searching

--- parse.py
+++ parse.py
@@ -3,12 +3,25 @@
 """
 
 from metapub import PubMedFetcher
+import pandas as pd
 
-fetch = PubMedFetch()
+fetch = PubMedFetcher()
 
-pmids = getch.pmids_for_query(query='13C glucose flux histone PTM metabolism',
+# Get PMIDs using query
+pmids = fetch.pmids_for_query(query='"chromatin" "metabolism" "humans" "histones/metabolism"',
                               since=None,
                               until=None,
                               retmax=1000)
-
-print(pmids)
+print("Number of PMIDs with search query: " + str(len(pmids)))
+#print("Example PMID Queried: " + str(pmids[4]))
+# Get abstracts based on keyword search
+abstracts = {}
+for id in pmids:
+    article = fetch.article_by_pmid(id)
+    abstracts[id] = [article.title, article.abstract, article.journal, article.year, article.authors]
+    #print(abstracts[id])
+df = pd.DataFrame.from_dict(abstracts,
+                            orient='index',
+                            columns=['Title', 'Abstract', 'Journal', 'Year', 'Authors'])
+df.index.name = 'PMID'
+df.to_csv('abstracts.csv')
